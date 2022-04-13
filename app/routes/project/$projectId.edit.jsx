c555c38f35a4c8e9c670d636f1283f2a5e2ffa01
Jill Fetherston <jfetherston@boxed.com>
2022-04-13 13:22:45 -0400

    working action flow

--- app/routes/project/$projectId.edit.jsx
+++ app/routes/project/$projectId.edit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { Form } from "@remix-run/react";
 import { useLoaderData, json, Link, Outlet, redirect } from 'remix';
 
 export const loader = async ({ params }) => {
@@ -17,17 +17,16 @@ export const loader = async ({ params }) => {
 }
 
 export const action = async ({ request, params }) => {
-  console.log('params', params);
+  // console.log('params', params);
   const user = process.env.USERNAME;
   const password = process.env.PASSKEY;
 
   var myHeaders = new Headers();
-      myHeaders.append('Authorization', 'Basic ' + btoa(`${user}:${password}`));
+  myHeaders.append('Authorization', 'Basic ' + btoa(`${user}:${password}`));
 
   const formData = new FormData();
-
-      formData.append("name", "julissa 2");
-      formData.append("made_for", "mee");
+  formData.append("name", "sfalsdkjfsdlkj");
+  formData.append("made_for", "mee");
 
   var requestOptions = {
     method: 'POST',
@@ -35,36 +34,25 @@ export const action = async ({ request, params }) => {
     body: formData,
     redirect: 'follow'
   };
-
-  fetch("https://api.ravelry.com/projects/noone1200/30041536.json", requestOptions)
+  const res = await fetch("https://api.ravelry.com/projects/noone1200/30041536.json", requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
     .catch(error => console.log('error', error));
-    return {};
-
-
+  return redirect(`/projects`);
 }
+
 function editPost() {
   const { project } = useLoaderData();
-
-  const { control, handleSubmit, register, watch, formState: { errors } } = useForm({
-    defaultValue: {
-      name: project.name,
-      made_for: project.made_for,
-    }
-  });
 
   return (
     <div className='edit-form-wrapper'>
       <Link to='/projects'>Back</Link>
-      <form method='post'>
-        {/* <form method='POST'> */}
+      <Form method='post'>
         <label htmlFor='name'>Project Name</label>
-        <input defaultValue={project.name} {...register('name')} name='name' required={true} />
+        <input defaultValue={project.name} name='name' required={true} />
         <label htmlFor='name'>Made For</label>
-        <input defaultValue={project.made_for} {...register('made_for')} name='made_for' />
+        <input defaultValue={project.made_for} name='made_for' />
         <button className='button' type='submit'>Update Project</button>
-      </form>
+      </Form>
     </div>
   )
 }
