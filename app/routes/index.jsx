07ebd3dc91c2d6af19204d3af0f2bf7dedb6242c
Jill Fetherston <jfetherston@boxed.com>
2022-04-17 22:06:25 -0400

    add and index page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,7 @@
 import { response } from 'express';
 import { useEffect } from 'react';
 import { useLoaderData, json, Link, Outlet } from 'remix';
-import styles from '~/routes/projects/styles.css';
+import styles from '../styles/home';
 import { ImageList, ImageListItem, ImageListItemBar, IconButton } from '@mui/material';
 
 export const loader = async () => {
@@ -27,11 +27,14 @@ function Home() {
   console.log('user', user);
 
   return (
-    <div className='projectsRoute'>
+    <div className='homeRoute'>
       <h1>{ user.username }'s Rav</h1>
       <div className='avatar-wrapper'>
         <img src={user.large_photo_url} height={300} width={300}/>
       </div>
+      <p>It's also a quick walk around Remix - check out some routing, admire some error boundary'ing. </p>
+      <p>This is a home page - the ol' '/'. Every Remix app has to have one and it can't have any child routes.</p>
+      <p>When asked, Remix says if you need to put a child route here, you don't really need to put a child route here.</p>
     </div>
   )
 }
